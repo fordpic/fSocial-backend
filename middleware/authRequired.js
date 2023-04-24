b94cf21ee20a4b50f883013f98d5d8e910f623ca
Ford Pickert <fordpickert@gmail.com>
2023-04-24 13:34:55 -0500

    auth middleware first pass complete

--- middleware/authRequired.js
+++ middleware/authRequired.js
@@ -3,4 +3,23 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-const authRequired = () => {};
+const authRequired = (req, res, next) => {
+	const bearerHeader = req.headers['authorization'];
+	console.log(bearerHeader);
+
+	if (typeof bearerHeader !== 'undefined') {
+		const token = bearerHeader.split(' ')[1];
+		jwt.verify(token, process.env.JWT_SECRET, function (err, payload) {
+			console.log('JWT Payload: ', payload);
+
+			if (err) res.sendStatus(500);
+
+			req.currentUser = payload.id;
+			next();
+		});
+	} else {
+		res.sendStatus(403);
+	}
+};
+
+export default authRequired;
